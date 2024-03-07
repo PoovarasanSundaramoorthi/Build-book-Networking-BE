535749993ce132301415160f22ba8ca2cbc4941d
Poovarasan S <112718552+PoovarasanSundaramoorthi@users.noreply.github.com>
2024-03-07 17:53:57 +0530

    Add files via upload

---
+++ appError.js
@@ -0,0 +1,8 @@
+export default class AppError extends Error {
+  constructor(statusCode, status, message) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = status;
+    this.message = message;
+  }
+}
