568549b15e91b020f408e467d8b4cc13d37fa9a5
Poovarasan S <112718552+PoovarasanSundaramoorthi@users.noreply.github.com>
2024-03-07 17:57:21 +0530

    Delete appError.js

--- appError.js
+++
@@ -1,8 +0,0 @@
-export default class AppError extends Error {
-  constructor(statusCode, status, message) {
-    super(message);
-    this.statusCode = statusCode;
-    this.status = status;
-    this.message = message;
-  }
-}
