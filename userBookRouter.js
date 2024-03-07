12b7a0b2182cb8e7cca2c65b7749928f9d5ee8b1
Poovarasan S <112718552+PoovarasanSundaramoorthi@users.noreply.github.com>
2024-03-07 17:59:47 +0530

    Delete userBookRouter.js

--- userBookRouter.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { CreateUserBook, getUserIdBooks } from '../controllers/bookUserController.js';
-
-const router = Router();
-router.route('/:id').post(CreateUserBook);
-router.route('/:userId').get(getUserIdBooks);
-export default router;
